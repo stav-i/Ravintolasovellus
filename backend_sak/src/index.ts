7742d2f549832698ec5841dbdab1f201162e4965
Tomi <tomi_99@hotmail.fi>
2023-11-17 16:15:02 +0200

    works

--- backend_sak/src/index.ts
+++ backend_sak/src/index.ts
@@ -95,7 +95,7 @@ app.get("/api", (req, res) => {
 });
 
 app.listen(PORT2, () => {
-  console.log(`Server is running on http://localhost:${PORT2}`);
+  console.log(`Create account server is running on http://localhost:${PORT2} :O`);
 });
 
 //Sisäänkirjautuminen
